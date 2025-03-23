2c71ec3c3ae99da71022ee242276b834f356eb0c
Suhas Bachhav <ksb.suhas@gmail.com>
2025-03-23 21:38:15 +0530

    counter value dispatch

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,15 +1,33 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const counter = useSelector(state => state.counter);
 
   const toggleCounterHandler = () => {};
+
+  const incrementHandler = () => {
+    dispatch({ type: 'INCREMENT' });
+  };
+
+  const decrementHandler = () => {
+    dispatch({ type: 'DECREMENT' });
+  };
+
+  const resetHandler = () => {
+    dispatch({ type: 'RESET' });
+  };
   
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{counter}</div>
+      <div>
+        <button onClick={incrementHandler}>Increment</button>
+        <button onClick={decrementHandler}>Decrement</button>
+        <button onClick={resetHandler}>Reset</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
--- src/store/index.js
+++ src/store/index.js
@@ -4,11 +4,11 @@ const counterReducer = (state = {counter: 0}, action) => {
   switch (action.type) {
     case 'INCREMENT':
       return {
-        counter: state.count + 1
+        counter: state.counter + 1
       };
     case 'DECREMENT':
       return {
-        counter: state.count - 1
+        counter: state.counter - 1
       };
     case 'RESET':
       return {
